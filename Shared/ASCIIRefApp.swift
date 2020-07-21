a130b35ab7f820b24a2f36c108e5ce6283afcc67
Kristopher Johnson <kris@kristopherjohnson.net>
2020-07-21 18:25:32 -0400

    Initial Commit

---
+++ Shared/ASCIIRefApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASCIIRefApp.swift
+//  Shared
+//
+//  Created by Kristopher Johnson on 7/21/20.
+//
+
+import SwiftUI
+
+@main
+struct ASCIIRefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
